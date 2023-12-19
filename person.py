5d1b52cb9498b09b938b9b256d4eef61003ebdd8
Frederick Morrison <frederick.morrison@outlook.com>
2023-12-19 13:48:17 -0700

    initialize search_string in post init method

--- person.py
+++ person.py
@@ -16,3 +16,7 @@ class Person:
     email_addresses: list[str] = field(default_factory=list)
     # NOTE: init=False prevents callers from providing their own ID
     id: str = field(init=False, default_factory=generate_id)
+    search_string: str = field(init=False)
+
+    def __post_init__(self) -> None:
+        self.search_string = f'{self.name} {self.address}'
