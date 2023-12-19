f8a0b94d2e3801cb9ff14f8948bfc36c8d108e73
Frederick Morrison <frederick.morrison@outlook.com>
2023-12-19 13:51:53 -0700

    exclude search string field from repr output

--- person.py
+++ person.py
@@ -16,7 +16,7 @@ class Person:
     email_addresses: list[str] = field(default_factory=list)
     # NOTE: init=False prevents callers from providing their own ID
     id: str = field(init=False, default_factory=generate_id)
-    search_string: str = field(init=False)
+    _search_string: str = field(init=False, repr=False)
 
     def __post_init__(self) -> None:
-        self.search_string = f'{self.name} {self.address}'
+        self._search_string = f'{self.name} {self.address}'
